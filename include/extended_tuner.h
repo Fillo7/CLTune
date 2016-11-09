daae16cd4e4b7bbb2a4a0b8e15c980d6e0093609
Fillo <mail.filip.petrovic@gmail.com>
2016-11-09 19:36:29 +0100

    Extended tuner now provides wrappers for basic tuner methods.

--- include/extended_tuner.h
+++ include/extended_tuner.h
@@ -17,26 +17,97 @@
 namespace cltune
 {
 
-using UniqueTuner = std::unique_ptr<Tuner>;
 using UniqueConfigurator = std::unique_ptr<TunerConfigurator>;
 
 class ExtendedTuner
 {
 public:
-    // Initializes the extended tuner by providing basic tuner (with already set arguments, parameters, etc.) and its configurator.
-    explicit PUBLIC_API ExtendedTuner(UniqueTuner basicTuner, UniqueConfigurator configurator);
+    // Initializes the extended tuner by providing platform id, device id and configurator class.
+    explicit PUBLIC_API ExtendedTuner(size_t platformId, size_t deviceId, UniqueConfigurator configurator);
     
     // Extended tuner destructor.
     PUBLIC_API ~ExtendedTuner();
 
+    // Adds a new kernel to the list of tuning-kernels and returns a unique ID (to be used when adding tuning parameters).
+    // Either loads the source from filenames or from string.
+    size_t PUBLIC_API addKernel(const std::vector<std::string>& filenames, const std::string& kernelName, const IntRange& global,
+                                const IntRange& local);
+    size_t PUBLIC_API addKernelFromString(const std::string& source, const std::string& kernelName, const IntRange& global, const IntRange& local);
+
+    // Sets the reference kernel. Same as the AddKernel function, but in this case there is only one reference kernel.
+    // Calling this function again will overwrite the previous reference kernel.
+    void PUBLIC_API setReference(const std::vector<std::string>& filenames, const std::string& kernelName, const IntRange& global,
+                                 const IntRange& local);
+    void PUBLIC_API setReferenceFromString(const std::string& source, const std::string& kernelName, const IntRange& global, const IntRange& local);
+
+    // Adds a new tuning parameter for a kernel with a specific ID. The parameter has a name, the number of values, and a list of values.
+    void PUBLIC_API addParameter(const size_t id, const std::string& parameterName, const std::initializer_list<size_t>& values);
+
+    // As above, but now adds a single valued parameter to the reference
+    void PUBLIC_API addParameterReference(const std::string& parameterName, const size_t value);
+
+    // Modifies the global or local thread-size (integers) by one of the parameters (strings). The modifier can be multiplication or division.
+    void PUBLIC_API mulGlobalSize(const size_t id, const StringRange range);
+    void PUBLIC_API divGlobalSize(const size_t id, const StringRange range);
+    void PUBLIC_API mulLocalSize(const size_t id, const StringRange range);
+    void PUBLIC_API divLocalSize(const size_t id, const StringRange range);
+
+    // Sets number of iterations that multirun kernel has to complete in order to produce complete result. Number of iterations is specified
+    // by previously added tuning parameter, which allows it to become part of the tuning process. Single run kernels do not need to use this method.
+    void PUBLIC_API setMultirunKernelIterations(const size_t id, const std::string& parameterName);
+
+    // Adds a new constraint to the set of parameters (e.g. must be equal or larger than). The constraints come in the form of a function object
+    // which takes a number of tuning parameters, given as a vector of strings (parameter names). Their names are later substituted by actual values.
+    void PUBLIC_API addConstraint(const size_t id, ConstraintFunction validIf, const std::vector<std::string>& parameters);
+
+    // As above, but for local memory usage. If this function is not called, it is assumed that the
+    // local memory usage is 0: no configurations will be excluded because of too much local memory.
+    void PUBLIC_API setLocalMemoryUsage(const size_t id, LocalMemoryFunction amount, const std::vector<std::string>& parameters);
+
+    // Functions to add kernel-arguments for input buffers, output buffers, and scalars. Make sure to
+    // call these in the order in which the arguments appear in the kernel.
+    template <typename T> void addArgumentInput(const size_t id, const std::vector<T>& source);
+    template <typename T> void addArgumentOutput(const size_t id, const std::vector<T>& source);
+    template <typename T> void addArgumentScalar(const size_t id, const T argument);
+
+    // Same as above, but for reference kernel.
+    template <typename T> void addArgumentInputReference(const std::vector<T>& source);
+    template <typename T> void addArgumentOutputReference(const std::vector<T>& source);
+    template <typename T> void addArgumentScalarReference(const T argument);
+
+    // Configures a specific search method. The default search method is "FullSearch". These are implemented as separate functions since
+    // they each take a different number of arguments.
+    void PUBLIC_API useFullSearch();
+    void PUBLIC_API useRandomSearch(const double fraction);
+    void PUBLIC_API useAnnealing(const double fraction, const double maxTemperature);
+    void PUBLIC_API usePSO(const double fraction, const size_t swarmSize, const double influenceGlobal, const double influenceLocal,
+                           const double influenceRandom);
+
+    // Uses chosen technique for results comparison. Currently available techniques are absolute difference and side by side comparison.
+    void PUBLIC_API chooseVerificationTechnique(const VerificationTechnique technique);
+    void PUBLIC_API chooseVerificationTechnique(const VerificationTechnique technique, const double toleranceTreshold);
+
+    // Outputs the search process to a file
+    void PUBLIC_API outputSearchLog(const std::string& filename);
+
+    // Trains a machine learning model based on the search space explored so far. Then, all the missing data-points are estimated
+    // based on this model. This is only useful if a fraction of the search space is explored, as is the case when doing random-search.
+    void PUBLIC_API modelPrediction(const Model modelType, const float validationFraction, const size_t testTopXConfigurations);
+
+    // Prints the results of the tuning either to screen (stdout) or to a specific output-file. Returns the execution time in miliseconds.
+    double PUBLIC_API printToScreen() const;
+    void PUBLIC_API printFormatted() const;
+    void PUBLIC_API printJSON(const std::string& filename, const std::vector<std::pair<std::string, std::string>>& descriptions) const;
+    void PUBLIC_API printToFile(const std::string& filename) const;
+
     // Runs single kernel using the provided configurator.
-    PUBLIC_API void runSingleKernel(const size_t id, const ParameterRange &parameter_values);
+    PUBLIC_API void runSingleKernel(const size_t id, const ParameterRange& parameterValues);
 
     // Starts tuning process using the provided configurator.
     PUBLIC_API void tune();
 
 private:
-    UniqueTuner basicTuner;
+    std::unique_ptr<Tuner> basicTuner;
     UniqueConfigurator configurator;
 };
 
--- src/extended_tuner.cpp
+++ src/extended_tuner.cpp
@@ -8,22 +8,181 @@
 namespace cltune
 {
 
-    ExtendedTuner::ExtendedTuner(UniqueTuner basicTuner, UniqueConfigurator configurator)
+    ExtendedTuner::ExtendedTuner(size_t platformId, size_t deviceId, UniqueConfigurator configurator):
+        basicTuner(new Tuner(platformId, deviceId))
     {
-        this->basicTuner = std::move(basicTuner);
         this->configurator = std::move(configurator);
     } 
 
     ExtendedTuner::~ExtendedTuner() {}
 
-    void ExtendedTuner::runSingleKernel(const size_t id, const ParameterRange &parameter_values)
+    size_t ExtendedTuner::addKernel(const std::vector<std::string>& filenames, const std::string& kernelName, const IntRange& global,
+                                    const IntRange& local)
+    {
+        return basicTuner->AddKernel(filenames, kernelName, global, local);
+    }
+
+    size_t ExtendedTuner::addKernelFromString(const std::string& source, const std::string& kernelName, const IntRange& global,
+                                              const IntRange& local)
+    {
+        return basicTuner->AddKernelFromString(source, kernelName, global, local);
+    }
+
+    void ExtendedTuner::setReference(const std::vector<std::string>& filenames, const std::string& kernelName, const IntRange& global,
+                                     const IntRange& local)
+    {
+        basicTuner->SetReference(filenames, kernelName, global, local);
+    }
+
+    void ExtendedTuner::setReferenceFromString(const std::string& source, const std::string& kernelName, const IntRange& global,
+                                               const IntRange& local)
+    {
+        basicTuner->SetReferenceFromString(source, kernelName, global, local);
+    }
+
+    void ExtendedTuner::addParameter(const size_t id, const std::string& parameterName, const std::initializer_list<size_t>& values)
+    {
+        basicTuner->AddParameter(id, parameterName, values);
+    }
+
+    void ExtendedTuner::addParameterReference(const std::string& parameterName, const size_t value)
+    {
+        basicTuner->AddParameterReference(parameterName, value);
+    }
+
+    void ExtendedTuner::mulGlobalSize(const size_t id, const StringRange range)
+    {
+        basicTuner->MulGlobalSize(id, range);
+    }
+
+    void ExtendedTuner::divGlobalSize(const size_t id, const StringRange range)
+    {
+        basicTuner->DivGlobalSize(id, range);
+    }
+
+    void ExtendedTuner::mulLocalSize(const size_t id, const StringRange range)
+    {
+        basicTuner->MulLocalSize(id, range);
+    }
+
+    void ExtendedTuner::divLocalSize(const size_t id, const StringRange range)
+    {
+        basicTuner->DivLocalSize(id, range);
+    }
+
+    void ExtendedTuner::setMultirunKernelIterations(const size_t id, const std::string& parameterName)
+    {
+        basicTuner->SetMultirunKernelIterations(id, parameterName);
+    }
+
+    void ExtendedTuner::addConstraint(const size_t id, ConstraintFunction validIf, const std::vector<std::string>& parameters)
+    {
+        basicTuner->AddConstraint(id, validIf, parameters);
+    }
+
+    void ExtendedTuner::setLocalMemoryUsage(const size_t id, LocalMemoryFunction amount, const std::vector<std::string>& parameters)
+    {
+        basicTuner->SetLocalMemoryUsage(id, amount, parameters);
+    }
+
+    template <typename T> void ExtendedTuner::addArgumentInput(const size_t id, const std::vector<T> &source)
+    {
+        basicTuner->AddArgumentInput(id, source);
+    }
+
+    template <typename T> void ExtendedTuner::addArgumentOutput(const size_t id, const std::vector<T> &source)
+    {
+        basicTuner->AddArgumentOutput(id, source);
+    }
+
+    template <typename T> void ExtendedTuner::addArgumentScalar(const size_t id, const T argument)
+    {
+        basicTuner->AddArgumentScalar(id, argument);
+    }
+
+    template <typename T> void ExtendedTuner::addArgumentInputReference(const std::vector<T>& source)
+    {
+        basicTuner->AddArgumentInputReference(source);
+    }
+
+    template <typename T> void ExtendedTuner::addArgumentOutputReference(const std::vector<T>& source)
+    {
+        basicTuner->AddArgumentOutputReference(source);
+    }
+    template <typename T> void ExtendedTuner::addArgumentScalarReference(const T argument)
+    {
+        basicTuner->AddArgumentScalarReference(argument);
+    }
+
+    void ExtendedTuner::useFullSearch()
+    {
+        basicTuner->UseFullSearch();
+    }
+
+    void ExtendedTuner::useRandomSearch(const double fraction)
+    {
+        basicTuner->UseRandomSearch(fraction);
+    }
+
+    void ExtendedTuner::useAnnealing(const double fraction, const double maxTemperature)
+    {
+        basicTuner->UseAnnealing(fraction, maxTemperature);
+    }
+
+    void ExtendedTuner::usePSO(const double fraction, const size_t swarmSize, const double influenceGlobal, const double influenceLocal,
+                               const double influenceRandom)
+    {
+        basicTuner->UsePSO(fraction, swarmSize, influenceGlobal, influenceLocal, influenceRandom);
+    }
+
+    void ExtendedTuner::chooseVerificationTechnique(const VerificationTechnique technique)
+    {
+        basicTuner->ChooseVerificationTechnique(technique);
+    }
+
+    void ExtendedTuner::chooseVerificationTechnique(const VerificationTechnique technique, const double toleranceTreshold)
+    {
+        basicTuner->ChooseVerificationTechnique(technique, toleranceTreshold);
+    }
+
+    void ExtendedTuner::outputSearchLog(const std::string &filename)
+    {
+        basicTuner->OutputSearchLog(filename);
+    }
+
+    void ExtendedTuner::modelPrediction(const Model modelType, const float validationFraction, const size_t testTopXConfigurations)
+    {
+        basicTuner->ModelPrediction(modelType, validationFraction, testTopXConfigurations);
+    }
+
+    double ExtendedTuner::printToScreen() const
+    {
+        return basicTuner->PrintToScreen();
+    }
+
+    void ExtendedTuner::printFormatted() const
+    {
+        basicTuner->PrintFormatted();
+    }
+
+    void ExtendedTuner::printJSON(const std::string& filename, const std::vector<std::pair<std::string, std::string>>& descriptions) const
+    {
+        basicTuner->PrintJSON(filename, descriptions);
+    }
+
+    void ExtendedTuner::printToFile(const std::string& filename) const
+    {
+        basicTuner->PrintToFile(filename);
+    }
+
+    void ExtendedTuner::runSingleKernel(const size_t id, const ParameterRange& parameterValues)
     {
         auto beforeTuningBegin = std::chrono::high_resolution_clock::now();
         configurator->beforeTuning();
         auto beforeTuningEnd = std::chrono::high_resolution_clock::now();
         auto beforeDuration = std::chrono::duration_cast<std::chrono::milliseconds>(beforeTuningEnd - beforeTuningBegin).count();
 
-        float kernelDuration = basicTuner->RunSingleKernel(id, parameter_values);
+        float kernelDuration = basicTuner->RunSingleKernel(id, parameterValues);
 
         auto afterTuningBegin = std::chrono::high_resolution_clock::now();
         configurator->afterTuning();
